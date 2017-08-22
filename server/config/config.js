72b818dbc0e55be7c2c64f577da90714065b4b3c
Jochen J.J.J <jochenvervoort@gmail.com>
2017-08-22 16:35:51 +0200

    Update config to use json file

--- server/config/config.js
+++ server/config/config.js
@@ -1,10 +1,23 @@
 var env = process.env.NODE_ENV || 'development';
 console.log('env *********', env);
 
-if (env === 'development') {
-   process.env.PORT = 3000;
-   process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
-} else if (env === 'test') {
-   process.env.PORT = 3000;
-   process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
+if (env === 'development' || env === 'test') {
+   var config = require('./config.json');
+   var envConfig = config[env];
+
+   // Object.keys(envConfig); // 'PORT', 'MONGODB_URI'
+   // console.log(Object.keys(envConfig));
+   Object.keys(envConfig).forEach((key) => {
+      process.env[key] = envConfig[key];
+   });
 }
+
+
+// Oud, dit staat nu allemaal in config.JSON
+// if (env === 'development') {
+//    process.env.PORT = 3000;
+//    process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
+// } else if (env === 'test') {
+//    process.env.PORT = 3000;
+//    process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
+// }
