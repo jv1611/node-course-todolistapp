d7aa325400be94713b534fc42406dc5392bbe3ab
Jochen J.J.J <jochenvervoort@gmail.com>
2017-08-30 16:37:21 +0200

    Update config to use json file

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,11 +1,16 @@
 const mongoose = require('mongoose');
 
+
+
+
+
 mongoose.Promise = global.Promise;
-// mongoose.connect('mongodb://jochen:nodejochen@ds055842.mlab.com:55842/nodecursusandrew' || 'mongodb://localhost:27017/TodoApp', {
+// mongoose.connect('mongodb://testuser1:testjoch@ds055842.mlab.com:55842/nodecursusandrew' || 'mongodb://localhost:27017/TodoApp', {
 // mongoose.connect('mongodb://jochen:nodejochen@ds055842.mlab.com:55842/nodecursusandrew' Dit kan weggehaald worden vanaf Les86
 // Zou moeten zijn: mongoose.connect(process.env.MONGODB_URI);
-mongoose.connect('mongodb://localhost:27017/TodoApp', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp', {
 // Zou moeten zijn: mongoose.connect(process.env.MONGODB_URI);
+// Op grond van antwoord 'Credit card' hier PROD_MONGODB
    useMongoClient: true
 });
 
